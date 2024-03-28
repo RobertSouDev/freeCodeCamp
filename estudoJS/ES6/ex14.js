39fdd0b440ce6829278e70c2f3b65a3a1cad1295
RobertSouDev <rogerhallks1@gmail.com>
2024-03-27 23:32:39 -0300

    questao 14

--- estudoJS/ES6/ex14.js
+++ estudoJS/ES6/ex14.js
@@ -28,5 +28,19 @@ const stats = {
   };
   
   // Altere apenas o código abaixo desta linha
-  const half = (stats) => (stats.max + stats.min) / 2.0; 
+  //const half = (stats) => (stats.max + stats.min) / 2.0; 
   // Altere apenas o código acima desta linha
+
+  //resposta 
+    const stats = {
+      max: 56.78,
+      standard_deviation: 4.34,
+      median: 34.54,
+      mode: 23.87,
+      min: -0.75,
+      average: 35.85
+    };
+    
+    // Altere apenas o código abaixo desta linha
+    const half = ({max,min}) => (max+ min ) /2 
+    // Altere apenas o código acima desta linha
